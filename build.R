02b51ea2f5cd7e65c704c315652203fbdd1af0e4
Nan Xiao <me@nanx.me>
2022-01-31 04:05:07 -0500

    Add reset step for build script

--- build.R
+++ build.R
@@ -1,5 +1,10 @@
 # Preview
-blogdown::build_site(local = TRUE); blogdown::serve_site()
+blogdown::build_site(local = TRUE)
+blogdown::serve_site()
+
+# Reset
+fs::file_delete(fs::dir_ls("public/", type = "directory"))
+fs::file_delete(fs::dir_ls("public/", type = "file"))
 
 # Deploy
 blogdown::build_site(local = FALSE)
